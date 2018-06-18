a033115f906ec7608c482d9d4ac5bb3f603599a4
Ivan <rovany706@gmail.com>
2018-06-19 00:57:14 +0500

    Add solution

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -3,13 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InputHelper;
 
 namespace Task7
 {
     class Program
     {
+        //проверка длин кодовых слов по неравенству Макмиллана
+        static bool CheckLengthsAndFindMax(int[] lengths, out int max)
+        {
+            double sum = 0;
+            max = lengths[0];
+            foreach (int num in lengths)
+            {
+                if (num > max)
+                    max = num;
+                sum += 1 / Math.Pow(3, num);
+            }
+            if (sum <= 1)
+                return true;
+            return false;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Задача 7\n=================");
+            Console.WriteLine("Условие задачи:\nПостроить префиксный троичный код с заданными длинами кодовых слов.\n" +
+                              "Кодовые слова выписать в лексикографическом порядке.\n" +
+                              "=================");
+            bool isOk;
+            int[] lengthsOfWords;
+            int maxLength;
+            do
+            {
+                lengthsOfWords = Input.ReadIntLine("Введите длины кодовых слов, разделяя их пробелом: ");
+                isOk = CheckLengthsAndFindMax(lengthsOfWords, out maxLength);
+                if (!isOk)
+                    Console.WriteLine("Ошибка! Введенные длины кодовых слов не прошли проверку по неравенству Макмиллана. Повторите ввод.");
+            } while (!isOk);
+
+            lengthsOfWords = lengthsOfWords.OrderBy(num => num).ToArray();
+
+            Tree tree = new Tree();
+            Tree.GenerateTree(tree.root, 0, maxLength);
+
+            foreach (int length in lengthsOfWords)
+            {
+                Tree.isGenereated = false;
+                Tree.GenerateEndpoints(tree.root, 0, length);
+            }
+
+            Tree.isGenereated = false;
+            Tree.words = new List<string>(lengthsOfWords.Length);
+            Tree.GenerateWords(tree.root, string.Empty);
+
+            Console.Write("Построенный префиксный троичный код: ");
+            foreach (string word in Tree.words)
+                Console.Write($"{word} ");
+            Console.WriteLine();
         }
     }
 }
---
+++ Task7/Tree.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Node
+    {
+        public int Number { get; set; }
+        public Node Left { get; set; } //0
+        public Node Middle { get; set; } //1
+        public Node Right { get; set; } //2
+        public bool IsEnd { get; set; } //занято ли
+
+        public Node(int number)
+        {
+            Number = number;
+            Left = Middle = Right = null;
+            IsEnd = false;
+        }
+    }
+    class Tree
+    {
+        public Tree()
+        {
+            root = new Node(-1);
+        }
+        public Node root { get; set; }
+        public static bool isGenereated;
+        public static List<string> words;
+
+        public static void GenerateTree(Node p, int currentLength, int length)
+        {
+            if (currentLength == 0)
+            {
+                if (p.Left == null)
+                {
+                    p.Left = new Node(0);
+                }
+
+                if (p.Middle == null)
+                {
+                    p.Middle = new Node(1);
+                }
+
+                if (p.Right == null)
+                {
+                    p.Right = new Node(2);
+                }
+            }
+            if (p != null && currentLength != length)
+            {
+                if (p.Left == null)
+                {
+                    p.Left = new Node(0);
+                }
+
+                if (p.Middle == null)
+                {
+                    p.Middle = new Node(1);
+                }
+
+                if (p.Right == null)
+                {
+                    p.Right = new Node(2);
+                }
+                GenerateTree(p.Left, currentLength + 1, length);
+                GenerateTree(p.Middle, currentLength + 1, length);
+                GenerateTree(p.Right, currentLength + 1, length);
+            }
+        }
+
+        public static void ShowTree(Node p, int l)
+        {
+            if (p != null)
+            {
+                ShowTree(p.Right, l + 5);
+                for (int i = 0; i < l; i++)
+                    Console.Write(" ");
+                Console.WriteLine("{0,10:F4}", p.Number);
+                ShowTree(p.Middle, l + 5);
+                ShowTree(p.Left, l + 5);
+            }
+        }
+
+        public static void GenerateEndpoints(Node p, int currentLength, int length)
+        {
+            if (!isGenereated)
+            {
+                if (p != null)
+                {
+                    if (currentLength == length - 1)
+                    {
+                        if (p.Left.IsEnd == false)
+                            p.Left.IsEnd = true;
+
+                        else if (p.Middle.IsEnd == false)
+                            p.Middle.IsEnd = true;
+
+                        else if (p.Right.IsEnd == false)
+                            p.Right.IsEnd = true;
+                        isGenereated = true;
+                    }
+
+                    else if (p.Left.IsEnd == false)
+                        GenerateEndpoints(p.Left, currentLength + 1, length);
+                    else if (p.Middle.IsEnd == false)
+                        GenerateEndpoints(p.Middle, currentLength + 1, length);
+                    else if (p.Right.IsEnd == false)
+                        GenerateEndpoints(p.Right, currentLength + 1, length);
+                }
+            }
+        }
+
+        public static void GenerateWords(Node p, string word)
+        {
+            if (p != null && p.IsEnd == false)
+            {
+                word += p.Number;
+                GenerateWords(p.Left, word);
+                GenerateWords(p.Middle, word);
+                GenerateWords(p.Right, word);
+            }
+            else if (p != null && p.IsEnd == true)
+            {
+                word += p.Number;
+                word = word.Remove(0, 2);//удалить -1
+                words.Add(word);
+            }
+        }
+    }
+}
